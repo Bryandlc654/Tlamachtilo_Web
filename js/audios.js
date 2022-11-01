31f8cb5ae63ac3ae0862c081b9cd57fa05f3e7eb
Bryan De la Cruz <bdelacruz654@gmail.com>
2022-11-01 13:31:32 -0500

    0.0.1

---
+++ js/audios.js
@@ -0,0 +1,12 @@
+var sound1 = new Audio();
+sound1.src = "./sounds/Hola.mp3";
+
+var sound2 = new Audio();
+sound2.src = "./sounds/Buen_día.mp3"
+
+var sound3 = new Audio();
+sound3.src = "./sounds/Buenas_tardes.mp3"
+
+var sound4 = new Audio();
+sound4.src = "./sounds/Buenas_noches.mp3"
+
