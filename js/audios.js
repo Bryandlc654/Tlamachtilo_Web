a740dc90fb764e116b6caf3b31f24c2357c6a6e7
Bryandlc654 <bdelacruz654@gmail.com>
2022-11-13 01:26:58 -0500

    0.0.22
    Nivel 5 completado

--- js/audios.js
+++ js/audios.js
@@ -1,3 +1,4 @@
+/* NIVEL 1 */
 var sound1_1 = new Audio();
 sound1_1.src = "./sounds/level1/Hola.mp3";
 
@@ -35,3 +36,159 @@ var sound1_12 = new Audio();
 sound1_12.src = "./sounds/level1/Luego_nos.mp3"
 
 
+/* NIVEL 2 */
+var sound2_1 = new Audio();
+sound2_1.src = "./sounds/level2/Platano.mp3";
+
+var sound2_2 = new Audio();
+sound2_2.src = "./sounds/level2/Calabaza.mp3"
+
+var sound2_3 = new Audio();
+sound2_3.src = "./sounds/level2/Chile.mp3"
+
+var sound2_4 = new Audio();
+sound2_4.src = "./sounds/level2/Naranja.mp3"
+
+var sound2_5 = new Audio();
+sound2_5.src = "./sounds/level2/Guayaba.mp3"
+
+var sound2_6 = new Audio();
+sound2_6.src = "./sounds/level2/Papa.mp3"
+
+var sound2_7 = new Audio();
+sound2_7.src = "./sounds/level2/Ejote.mp3"
+
+var sound2_8 = new Audio();
+sound2_8.src = "./sounds/level2/Aguacate.mp3"
+
+var sound2_9 = new Audio();
+sound2_9.src = "./sounds/level2/Tepejilote.mp3"
+
+var sound2_10 = new Audio();
+sound2_10.src = "./sounds/level2/Elote.mp3"
+
+var sound2_11 = new Audio();
+sound2_11.src = "./sounds/level2/Tomate.mp3"
+
+var sound2_12 = new Audio();
+sound2_12.src = "./sounds/level2/Chayote.mp3"
+
+
+/* NIVEL 3 */
+var sound3_1 = new Audio();
+sound3_1.src = "./sounds/level3/Boca.mp3";
+
+var sound3_2 = new Audio();
+sound3_2.src = "./sounds/level3/Brazo.mp3"
+
+var sound3_3 = new Audio();
+sound3_3.src = "./sounds/level3/Cabello.mp3"
+
+var sound3_4 = new Audio();
+sound3_4.src = "./sounds/level3/Cabeza.mp3"
+
+var sound3_5 = new Audio();
+sound3_5.src = "./sounds/level3/Cuello.mp3"
+
+var sound3_6 = new Audio();
+sound3_6.src = "./sounds/level3/Mano.mp3"
+
+var sound3_7 = new Audio();
+sound3_7.src = "./sounds/level3/Muslo.mp3"
+
+var sound3_8 = new Audio();
+sound3_8.src = "./sounds/level3/Nariz.mp3"
+
+var sound3_9 = new Audio();
+sound3_9.src = "./sounds/level3/Ojo.mp3"
+
+var sound3_10 = new Audio();
+sound3_10.src = "./sounds/level3/Pie.mp3"
+
+var sound3_11 = new Audio();
+sound3_11.src = "./sounds/level3/Rodilla.mp3"
+
+var sound3_12 = new Audio();
+sound3_12.src = "./sounds/level3/Uña.mp3"
+
+
+/* NIVEL 4 */
+var sound4_1 = new Audio();
+sound4_1.src = "./sounds/level4/";
+
+var sound4_2 = new Audio();
+sound4_2.src = "./sounds/level4/Botella.m4a"
+
+var sound4_3 = new Audio();
+sound4_3.src = "./sounds/level4/Collar.mp3"
+
+var sound4_4 = new Audio();
+sound4_4.src = "./sounds/level4/Comal.m4a"
+
+var sound4_5 = new Audio();
+sound4_5.src = "./sounds/level4/Espejo.m4a"
+
+var sound4_6 = new Audio();
+sound4_6.src = "./sounds/level4/Jarro.m4a"
+
+var sound4_7 = new Audio();
+sound4_7.src = "./sounds/level4/Libro.m4a"
+
+var sound4_8 = new Audio();
+sound4_8.src = "./sounds/level4/Metate.m4a"
+
+var sound4_9 = new Audio();
+sound4_9.src = "./sounds/level4/Olla.m4a"
+
+var sound4_10 = new Audio();
+sound4_10.src = "./sounds/level4/Papel.m4a"
+
+var sound4_11 = new Audio();
+sound4_11.src = "./sounds/level4/Peine.m4a"
+
+var sound4_12 = new Audio();
+sound4_12.src = "./sounds/level4/Ropa.m4a"
+
+
+/* NIVEL 5 */
+var sound5_1 = new Audio();
+sound5_1.src = "./sounds/level5/Abuela.mp3";
+
+var sound5_2 = new Audio();
+sound5_2.src = "./sounds/level5/Abuelo.mp3"
+
+var sound5_3 = new Audio();
+sound5_3.src = "./sounds/level5/Bebe.mp3"
+
+var sound5_4 = new Audio();
+sound5_4.src = "./sounds/level5/Bisabuela.mp3"
+
+var sound5_5 = new Audio();
+sound5_5.src = "./sounds/level5/Bisabuelo.mp3"
+
+var sound5_6 = new Audio();
+sound5_6.src = "./sounds/level5/Bisnieta.mp3"
+
+var sound5_7 = new Audio();
+sound5_7.src = "./sounds/level5/Hermana.mp3"
+
+var sound5_8 = new Audio();
+sound5_8.src = "./sounds/level5/Hermano.mp3"
+
+var sound5_9 = new Audio();
+sound5_9.src = "./sounds/level5/Madre.mp3"
+
+var sound5_10 = new Audio();
+sound5_10.src = "./sounds/level5/Nieto.mp3"
+
+var sound5_11 = new Audio();
+sound5_11.src = "./sounds/level5/Padre.mp3"
+
+var sound5_12 = new Audio();
+sound5_12.src = "./sounds/level5/Primo.mp3"
+
+var sound5_13 = new Audio();
+sound5_13.src = "./sounds/level5/Tia.mp3"
+
+var sound5_14 = new Audio();
+sound5_14.src = "./sounds/level5/Tio.mp3"
