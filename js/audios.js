de5a95e5029c3e294d9baabe7c02e5f0a59e3196
Bryandlc654 <bdelacruz654@gmail.com>
2022-11-27 14:40:48 -0500

    0.1.0

--- js/audios.js
+++ js/audios.js
@@ -1,39 +1,39 @@
 /* NIVEL 1 */
 var sound1_1 = new Audio();
-sound1_1.src = "./sounds/level1/Hola.mp3";
+sound1_1.src = "./sounds/level1/hola.mp4";
 
 var sound1_2 = new Audio();
-sound1_2.src = "./sounds/level1/Buen_día.mp3"
+sound1_2.src = "./sounds/level1/buen_dia.mp4"
 
 var sound1_3 = new Audio();
-sound1_3.src = "./sounds/level1/Buenas_tardes.mp3"
+sound1_3.src = "./sounds/level1/buenas_tardes.mp4"
 
 var sound1_4 = new Audio();
-sound1_4.src = "./sounds/level1/Buenas_noches.mp3"
+sound1_4.src = "./sounds/level1/buenas_noches.mp4"
 
 var sound1_5 = new Audio();
-sound1_5.src = "./sounds/level1/Cómo_estás.mp3"
+sound1_5.src = "./sounds/level1/como_estas.mp4"
 
 var sound1_6 = new Audio();
-sound1_6.src = "./sounds/level1/Que_haces.mp3"
+sound1_6.src = "./sounds/level1/que_haces.mp4"
 
 var sound1_7 = new Audio();
-sound1_7.src = "./sounds/level1/Donde_vas.mp3"
+sound1_7.src = "./sounds/level1/donde_vas.mp4"
 
 var sound1_8 = new Audio();
-sound1_8.src = "./sounds/level1/Bienvenido.mp3"
+sound1_8.src = "./sounds/level1/bienvenido.mp4"
 
 var sound1_9 = new Audio();
-sound1_9.src = "./sounds/level1/Gracias.mp3"
+sound1_9.src = "./sounds/level1/gracias.mp4"
 
 var sound1_10 = new Audio();
-sound1_10.src = "./sounds/level1/Hasta_la_próxima.mp3"
+sound1_10.src = "./sounds/level1/hasta_la_proxima.mp4"
 
 var sound1_11 = new Audio();
-sound1_11.src = "./sounds/level1/Hasta_mañana.mp3"
+sound1_11.src = "./sounds/level1/hasta_mañana.mp4"
 
 var sound1_12 = new Audio();
-sound1_12.src = "./sounds/level1/Luego_nos.mp3"
+sound1_12.src = "./sounds/level1/luego_nos_vemos.mp4"
 
 
 /* NIVEL 2 */
